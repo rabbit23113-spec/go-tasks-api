4af4d0cb6e82188901db815b3b0333bc919bce5a
Raffinnaisse <rabbit23113@gmail.com>
2026-05-28 10:56:46 +0300

    project: routes in handler.go

--- package/handler/handler.go
+++ package/handler/handler.go
@@ -1,1 +1,29 @@
 package handler
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Handler struct {
+	handler http.Handler
+}
+
+func (h *Handler) ServeRoutes() {
+	router := gin.New()
+	auth := router.Group("/auth")
+	{
+		auth.POST("/signup")
+		auth.POST("/signin")
+		auth.POST("/logout")
+	}
+	tasks := router.Group("/tasks")
+	{
+		tasks.GET("/")
+		tasks.GET("/:id")
+		tasks.POST("/")
+		tasks.PATCH("/:id")
+		tasks.DELETE("/:id")
+	}
+}
